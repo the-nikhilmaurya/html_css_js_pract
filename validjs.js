b60c729e061e0e67ddf5209546700e50405fd24b
Nikhil Maurya <nikhilmaurya2601@gmail.com>
2023-07-13 18:11:27 +0530

    validation pract js added

---
+++ validjs.js
@@ -0,0 +1,51 @@
+// this is going to validate my login credetials
+function validateForm(){
+    var email = document.forms['myform']['email'].value;
+    var password = document.forms['myform']['password'].value;
+    if((emailvalid(email) == true) &&( passwordvalid(password)) == true){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+    
+}
+
+function emailvalid(email){
+    if (email == ''){
+        document.getElementById('input-email').hidden = false;
+        return console.log("input email");
+    }
+    else if(email != "hello@gmail.com"){
+        document.getElementById('invalid-email').hidden = false;
+        return console.log("invalid email");
+    }else{
+        return true;
+    }
+}
+
+function passwordvalid(password){
+    if(password == ''){
+        document.getElementById('input-password').hidden = false;
+        return console.log("input password");
+    }
+    else if(password != '123'){
+        document.getElementById('invalid-password').hidden = false;
+        return console.log("invalid password");
+    }
+    else{
+        return true;
+    }
+}
+
+
+document.getElementById('input-email').hidden = true;
+document.getElementById('invalid-email').hidden = true;
+document.getElementById('input-password').hidden = true;
+document.getElementById('invalid-password').hidden = true;
+
+ function show() {
+
+    document.getElementById().hidden = false;
+ }
