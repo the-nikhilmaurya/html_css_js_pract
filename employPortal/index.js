216a04cb1c8824fc3e99d590cdbdd1935534dad0
Nikhil Maurya <nikhilmaurya2601@gmail.com>
2023-07-20 11:38:07 +0530

    validation  of id and age added

--- employPortal/index.js
+++ employPortal/index.js
@@ -18,27 +18,50 @@
         
     });
 
-
+    //funtion to validate given id
     function validateID(value)
     {
-        var table = document.getElementById("myTable").getElementsByTagName("tbody")[0];
-        for(i=0;i<table.rows.length;i++){
-            console.log("value = "+table.rows[i].cells[0].innerText==value)
-            if(table.rows[i].cells[0].innerText==value){
-                document.getElementById("isIdValid").innerHTML="ID is used"
+        // console.log(typeof value)
+        console.log("try "+Number.isInteger(parseInt(value)))
+
+        console.log(typeof value)
+        if(value == ""){
+            document.getElementById("isIdValid").innerHTML="Id can't be null"
+            return false;
+        }
+        else if(Number.isInteger(parseInt(value))==true){
+            let a =  parseInt(value)
+            if(check(a)==true){
+                document.getElementById("isIdValid").innerHTML=""
+                return true;
+            }
+            else{
+                document.getElementById("isIdValid").innerHTML="Id exist"
                 return false;
             }
-        }    
-        return true;     
+        }
+        else{
+            document.getElementById("isIdValid").innerHTML="Id must be a integer"
+            return false;
+        }
+
+        //will check in whether id is used previousaly or not
+        function check(value){
+            console.log("check = "+value)
+            for (i in primaryToken){
+                if(value == primaryToken[i]){
+                    return false;
+                }
+            }
+            return true;
+        }
         
     }
 
     function readData()         // this will help in reading the form data
     {
         var formdata = {}
-        formdata["id1"] = document.getElementById('id1').value;
-        primaryToken.push(formdata["id1"]); 
-
+        formdata["id1"] = document.getElementById('id1').value
         formdata["name"] = document.getElementById("name").value;
         formdata["age"] = document.getElementById("age").value;
         formdata["image"]=document.getElementById("image").value;
@@ -50,7 +73,10 @@
 
     function insertData(data)    // this will help in inserting the data in table
     {
-        if(validateID(data.id1)){
+        
+        // console.log("validdation"+validateID(data.id1))
+        if(validateID(data.id1)==true && validateAge(data.age)){
+        
         var table = document.getElementById("myTable").getElementsByTagName("tbody")[0];          
                 //[0] indicates the first tbody within the mytable
         var newRow = table.insertRow(table.length)
@@ -78,14 +104,47 @@
             cell6.innerHTML = `<td><button type="button" class="btn btn-success">VIEW</button>
             <button type="button" class="btn btn-secondary">EDIT</button>
             <button type="button" class="btn btn-danger">DELETE</button></td>`
+        primaryToken.push(data.id1)
         }
         else{
-            return;
+            return console.log("hi");
         }
 
     }
 
-    function resetForm()
+    function validateAge(value)
     {
-        document.getElementById('id1').innerText=""
+        if(value==""){
+            document.getElementById('isAgeValid').innerHTML="";
+            return true;
+        }
+        else if(Number.isInteger(parseInt(value))==true){
+            let a =  parseInt(value)
+            if(a>=18 && a<=60){
+                document.getElementById('isAgeValid').innerHTML="";         
+                return true;
+            }
+            else{
+                document.getElementById('isAgeValid').innerHTML="age must be within 18 and 60";
+            }
+        }
+        else{
+            document.getElementById('isAgeValid').innerHTML="Age must be Integer";
+        }
     }
+
+
+
+    // trypart
+    // if value.parse
+        // if(value == ""){
+
+        // }
+        // var table = document.getElementById("myTable").getElementsByTagName("tbody")[0];
+        // for(i=0;i<table.rows.length;i++){
+        //     console.log("value = "+table.rows[i].cells[0].innerText==value)
+        //     if(table.rows[i].cells[0].innerText==value){
+        //         document.getElementById("isIdValid").innerHTML="ID is used"
+        //         return false;
+        //     }
+        // } 
