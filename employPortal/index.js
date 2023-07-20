ae9675c90e1f4fd0024bde5989022aa8e734442e
Nikhil Maurya <nikhilmaurya2601@gmail.com>
2023-07-20 17:09:25 +0530

    completed search remaning

--- employPortal/index.js
+++ employPortal/index.js
@@ -7,18 +7,16 @@
         event.preventDefault();
         // event.stopPropagation();
         // console.log(readData());
-
-        console.log('dail')
         // var formdata = readData();
             
         // validateID(formdata.id1)
         var formdata = readData();
+        
         insertData(formdata);
-        resetForm();
+        
         
     });
 
-    //funtion to validate given id
     function validateID(value)
     {
         // console.log(typeof value)
@@ -75,7 +73,7 @@
     {
         
         // console.log("validdation"+validateID(data.id1))
-        if(validateID(data.id1)==true && validateAge(data.age)){
+        if(validateID(data.id1)==true && validateAge(data.age)==true){
         
         var table = document.getElementById("myTable").getElementsByTagName("tbody")[0];          
                 //[0] indicates the first tbody within the mytable
@@ -101,13 +99,14 @@
             cell5.innerHTML = data.designation;
             
         var cell6 = newRow.insertCell(6);
-            cell6.innerHTML = `<td><button type="button" class="btn btn-success">VIEW</button>
-            <button type="button" class="btn btn-secondary">EDIT</button>
-            <button type="button" class="btn btn-danger">DELETE</button></td>`
+            cell6.innerHTML = `<td>
+            <button type="button" onclick="onView(this)"  class="btn btn-success">VIEW</button>
+            <button type="button" onclick="onEdit(this)"   class="btn btn-secondary">EDIT</button>
+            <button type="button" onclick="onDelete(this)" class="btn btn-danger">DELETE</button></td>`
         primaryToken.push(data.id1)
         }
         else{
-            return console.log("hi");
+            return console.log("there might be some error");
         }
 
     }
@@ -115,26 +114,111 @@
     function validateAge(value)
     {
         if(value==""){
-            document.getElementById('isAgeValid').innerHTML="";
-            return true;
+            document.getElementById('isAgeValid').innerHTML="Age required";
+            document.getElementById('isAgeValid_edit').innerHTML="Age required";
+            return false;
         }
         else if(Number.isInteger(parseInt(value))==true){
             let a =  parseInt(value)
             if(a>=18 && a<=60){
                 document.getElementById('isAgeValid').innerHTML="";         
+                document.getElementById('isAgeValid_edit').innerHTML="";         
                 return true;
             }
             else{
                 document.getElementById('isAgeValid').innerHTML="age must be within 18 and 60";
+                document.getElementById('isAgeValid_edit').innerHTML="age must be within 18 and 60";
+                return false;
             }
         }
         else{
             document.getElementById('isAgeValid').innerHTML="Age must be Integer";
+            document.getElementById('isAgeValid_edit').innerHTML="Age must be Integer";
+            return false;
         }
     }
 
 
+    function onDelete(target)            // to delete the selected row
+    {
+        
+        var row = target.parentNode.parentNode;
+        
+        var table = row.parentNode;
+      
+        table.removeChild(row);
+        // console.log(primaryToken)
+    }
+    // to traverse through table cell index
+    var idx = null;
+    console.log("idx befoer= "+idx)
+    function onEdit(value)
+    {
+        idx=null
+        document.getElementById("editable").style.display='block';
+        document.getElementById("editable").style.width="400px";
+        console.log(value)
+        var table = document.getElementById("myTable").getElementsByTagName('tbody')[0];
+        var row = value.parentNode.parentNode;
+        // console.log(row.rowIndex)
 
+        idx = row.rowIndex-1;
+        document.getElementById("edit_id1").value = table.rows[idx].cells[0].innerText
+        document.getElementById("edit_name").value = table.rows[idx].cells[1].innerText
+        document.getElementById("edit_age").value = table.rows[idx].cells[2].innerText
+        document.getElementById("edit_image").value = table.rows[idx].cells[3].innerText
+        document.getElementById("edit_gender").value = table.rows[idx].cells[4].innerText
+        document.getElementById("edit_designation").value = table.rows[idx].cells[5].innerText
+
+        console.log("idx after= "+idx)
+    }
+
+    function update(){
+        event.preventDefault();
+        // console.log("idx update= "+idx)
+        var table = document.getElementById("myTable").getElementsByTagName('tbody')[0];
+        
+        table.rows[idx].cells[1].innerText=document.getElementById("edit_name").value
+        table.rows[idx].cells[2].innerText=document.getElementById("edit_age").value
+        if(validateAge(document.getElementById("edit_age").value)==true){
+            table.rows[idx].cells[3].innerText=document.getElementById("edit_image").value
+            table.rows[idx].cells[4].innerText=document.getElementById("edit_gender").value
+            table.rows[idx].cells[5].innerText=document.getElementById("edit_designation").value
+            document.getElementById("editable").style.display='none';
+        }else{
+        document.getElementById("editable").style.display='block';
+
+        }
+        // console.log(table.rows[idx].cells[0])
+        // document.getElementById("editable").style.display='none';
+    }
+
+
+    //view section
+    function onView(target){
+        var cellindx=null
+        document.getElementById("view").style.display='block';
+        var table = document.getElementById("myTable").getElementsByTagName('tbody')[0];
+        var row = target.parentNode.parentNode;
+        console.log(row.rowIndex)
+
+        cellindx = row.rowIndex-1;
+        document.getElementById("view_id1").value = table.rows[cellindx].cells[0].innerText
+        document.getElementById("view_name").value = table.rows[cellindx].cells[1].innerText
+        document.getElementById("view_age").value = table.rows[cellindx].cells[2].innerText
+        document.getElementById("selfImage").src = table.rows[cellindx].cells[3].innerText
+        
+        document.getElementById("view_gender").value = table.rows[cellindx].cells[4].innerText
+        document.getElementById("view_designation").value = table.rows[cellindx].cells[5].innerText
+
+        
+        console.log("cellindx after= "+cellindx)
+        // document.getElementById("view").style.display='none'
+    }
+
+    function viewDone(){
+        document.getElementById("view").style.display='none'
+    }
     // trypart
     // if value.parse
         // if(value == ""){
